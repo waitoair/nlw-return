798ff32d59c77cfa7ab41e53b47f7739e77d6cfe
Luana Saraiva <luana_saraiva2@hotmail.com>
2022-05-04 13:10:48 -0300

    initial config commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+  text?: string
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.text ?? 'Default'}</button>
+}
+
+function App() {
+  return (
+    <div>
+      <Button text="Enviar" />
+      <Button text="Ok" />
+      <Button />
+    </div>
+  )
+}
+
+export default App
